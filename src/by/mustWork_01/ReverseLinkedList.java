620ba31c4af883dcd07998f3a247fb92398d2d6b
naglec13 <naglec13@gmail.com>
2020-06-12 01:32:03 +0300

    .. linkedList reverse

---
+++ src/by/mustWork_01/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package by.mustWork_01;
+
+import java.util.*;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+        List<String> list = new LinkedList<String>();
+
+        System.out.println("list before filling : " + list);
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        System.out.println("list after setting : " + list);
+
+        System.out.println(list.size());
+
+
+        ListIterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()){
+            System.out.println("listIterator:" + iterator.next() );
+        }
+        System.out.println("######## Reverse");
+        while (iterator.hasPrevious()){
+            System.out.println("listIterator:" + iterator.previous() );
+        }
+    }
+}
